f0caedee0d5070a329a4ade1058f4b26a5f120ee
Andrewharber <harber.andrew@ymail.com>
2022-09-20 15:08:19 -0400

    commit tree f9d45a7f9e1253f126531f0e400ce018a3a08a58

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory';
+
+let body = document.querySelector('body');
+body.appendChild(newHeader)
+
+newHeader.textContent = "YOUR-NAME is the champion"
